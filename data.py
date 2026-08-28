5fa70d8692f5ccf97d26a39e086912a408fbbc34
Ode <126775407+lmaznursyahfajar@users.noreply.github.com>
2026-08-28 15:09:50 +0700

    Fix formatting in data.py

--- data.py
+++ data.py
@@ -49,7 +49,7 @@ SPOTS = [
                 ("Toilet umum (Area Parkir A & B)", "B · 70 m"),
                 ("Souvenir Shop", "T · 110 m"),
                 ("Pos Informasi & P3K", "T · 100 m")
-            ]
+            ],
         },
         "contact": {
             "phone": "0811 222 1284",
